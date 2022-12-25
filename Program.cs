a5d6d6ebe4af58d213b42a09abdf16c40083e5dc
mostafacode321 <121382386+mostafacode321@users.noreply.github.com>
2022-12-25 14:13:13 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+{
+  int n,i,sum;
+  int mn,mx;
+  
+	Console.Write("\n\n");
+    Console.Write("Find perfect numbers within a given number of range:\n");
+    Console.Write("------------------------------------------------------");
+    Console.Write("\n\n");    
+  
+  Console.Write("enter n1 range  : ");
+  mn = Convert.ToInt32(Console.ReadLine());  
+  Console.Write("enter n2 range  : ");
+  mx = Convert.ToInt32(Console.ReadLine());  
+  Console.Write("The Perfect numbers within the given range : ");
+  for(n=mn;n<=mx;n++)
+    {
+    i=1;
+    sum = 0;
+    while(i<n)
+	{
+      if(n%i==0)
+           sum=sum+i;
+          i++;
+    }
+    if(sum==n)
+      Console.Write("{0} ",n);
+    }
+      Console.Write("\n");
+ }  
+
